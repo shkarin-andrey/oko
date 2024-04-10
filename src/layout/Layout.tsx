fd581eb70fe57055488df08ef88e001de0a918b6
r09-Andrey <r09-andrey@monowork.pro>
2024-04-10 18:21:26 +0400

    feat: routing api

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,7 +3,7 @@ import { FC, useState } from 'react';
 import Header from '../components/Header';
 import Loader from '../components/Loader';
 import Navigation from '../components/Navigation';
-import { routing } from '../routers/routers.config';
+import { useGetProjectsQuery } from '../redux/api/apiProjectsSlice';
 import { ILoader } from './Loader.interface';
 
 const Layout: FC<ILoader> = ({ children, isLoading = false }) => {
@@ -15,6 +15,8 @@ const Layout: FC<ILoader> = ({ children, isLoading = false }) => {
 
   const classNameMobileMenu = isOpenMenu ? 'translate-x-0' : '-translate-x-full';
 
+  const { data: routing = [], isLoading: isLoadingRouting } = useGetProjectsQuery();
+
   return (
     <div className='h-screen overflow-hidden'>
       <Header isOpenMenu={isOpenMenu} onClick={handleClickHamburger} />
@@ -23,7 +25,13 @@ const Layout: FC<ILoader> = ({ children, isLoading = false }) => {
           className={`z-10 transition-transform duration-200 h-full lg:translate-x-0 overflow-y-auto min-w-[300px] border-r border-[#44537126] bg-[#F9FAFB] py-6 px-8 flex flex-col gap-[20px] absolute lg:static top-0 left-0 ${classNameMobileMenu}`}
         >
           <div className='text-[#858FA3]'>Проекты</div>
-          <Navigation routs={routing} />
+          {isLoadingRouting ? (
+            <div className='h-full flex justify-center items-center'>
+              <Loader />
+            </div>
+          ) : (
+            <Navigation routs={routing} />
+          )}
         </aside>
         <main className='w-full p-6 overflow-auto sm:overflow-hidden'>
           {isLoading ? (
--- src/routers/routers.config.ts
+++ src/routers/routers.config.ts
@@ -2,19 +2,3 @@ export const Paths = {
   root: '/',
   projectsDetail: 'projects/:id',
 } as const;
-
-export const routing = [
-  { value: 'ChatLab project', to: 63 },
-  { value: 'DevPlatform', to: 89 },
-  { value: 'Global Analytics', to: 107 },
-  { value: 'Slaver', to: 55 },
-  { value: 'Voip Lab', to: 69 },
-  { value: 'Analytics', to: 81 },
-  { value: 'Bot-Development', to: 80 },
-  { value: 'Motivation Project', to: 118 },
-  { value: 'Fakescreen', to: 68 },
-  { value: 'JSM-Bot', to: 47 },
-  { value: 'Design', to: 112 },
-  { value: 'FarmLab', to: 116 },
-  { value: 'Test Departament', to: 115 },
-];
